5796484136aa0f27918b6f6e0ebf84bf9fb7c6e0
andrewdonato <andyphobic@gmail.com>
2017-05-28 02:21:43 -0700

    installed apptimize sdk

---
+++ app/src/test/java/ABTest.java
@@ -0,0 +1,21 @@
+/**
+ * Created by andrewdonato on 5/28/17.
+ */
+
+
+import com.apptimize.Apptimize;
+import com.apptimize.ApptimizeTest;
+
+
+public class ABTest {
+
+    protected void onCreate(){
+
+        Apptimize.setup(this, "Bvqkgti87hsnh9XeXdrebyhggy8nWLc");
+
+        
+
+
+    }
+
+}
